fd17d4e46638b031fd3fb268f16c1dc8bf197561
Alex Abd <Alexander.Abdugafarov@nitka.com>
2015-10-18 02:09:05 +0500

    Catching socket timeout exception

--- src/main/scala/org/fs/rw/DetectionExecutor.scala
+++ src/main/scala/org/fs/rw/DetectionExecutor.scala
@@ -4,6 +4,7 @@ import org.fs.rw.domain.DetectionError
 import org.fs.rw.domain.Message
 import org.fs.rw.utility.Imports._
 import org.fs.rw.detection.Detector
+import java.net.SocketTimeoutException
 
 class DetectionExecutor(
     routerDiscoverer: RouterDiscoverer,
@@ -13,18 +14,30 @@ class DetectionExecutor(
     val routerIpFork = routerDiscoverer.discoverIp()
     routerIpFork match {
       case Right(routerIp) =>
-        detectors.toStream.flatMap(d =>
-          d.detect(routerIp, username, password)
-        ).headOption getOrElse {
-          DetectionError(
-            timestamp = now,
-            message = "Unknown router type"
-          )
-        }
+        detectWithIp(routerIp)(username, password)
       case Left(message) =>
         DetectionError(
           timestamp = now,
           message = message
+        )
+    }
+  }
+
+  private def detectWithIp(routerIp: String)(username: String, password: String): Message = {
+    try {
+      detectors.toStream.flatMap(d =>
+        d.detect(routerIp, username, password)
+      ).headOption getOrElse {
+        DetectionError(
+          timestamp = now,
+          message = "Unknown router type"
+        )
+      }
+    } catch {
+      case ex: SocketTimeoutException =>
+        DetectionError(
+          timestamp = now,
+          message = "Router query timeout"
         )
     }
   }
