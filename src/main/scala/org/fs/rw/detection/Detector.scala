722d0a68fdea88aa93ff360be65f87b163c509d7
Alex Abd <Alexander.Abdugafarov@nitka.com>
2015-10-23 03:29:25 +0500

    Reworked detection framework to save page content in case of failure

--- src/main/scala/org/fs/rw/detection/Detector.scala
+++ src/main/scala/org/fs/rw/detection/Detector.scala
@@ -1,10 +1,44 @@
 package org.fs.rw.detection
 
+import java.io.File
+import java.io.PrintWriter
+
 import org.fs.rw.domain.Message
+import org.fs.rw.utility.Imports._
 import org.slf4s.Logging
 
-import com.github.nscala_time.time.Imports._
-
 trait Detector extends Logging {
-  def detect(routerIp: String, username: String, password: String): Option[Message]
+  def detect(routerIp: String, username: String, password: String): Option[Message] = {
+    val content = getContent(routerIp, username, password)
+    content match {
+      case None =>
+        None
+      case Some(Left(message)) =>
+        Some(message)
+      case Some(Right(content)) =>
+        try {
+          Some(parseContent(content))
+        } catch {
+          case ex: Throwable =>
+            dump(content)
+            throw ex
+        }
+    }
+  }
+
+  def getContent(routerIp: String, username: String, password: String): Option[Either[Message, String]]
+
+  def parseContent(content: String): Message
+
+  private def dump(content: String): Unit = {
+    val filename = s"content-${getClass.getSimpleName}-${now.toString("yyyy-MM-dd_HH-mm")}.txt"
+    val file = new File(filename)
+    log.info(s"Dumped router content to ${file.getAbsolutePath}")
+    val pw = new PrintWriter(file, "UTF-8")
+    try {
+      pw.write(content)
+    } finally {
+      pw.close()
+    }
+  }
 }
