a47c7d27c141d157feba32b97ad8c5c3103c41ff
Alex Abd <Alexander.Abdugafarov@nitka.com>
2017-02-07 05:03:03 +0500

    Updated logged message and log file name

--- src/main/scala/org/fs/rw/RouterWatcherMain.scala
+++ src/main/scala/org/fs/rw/RouterWatcherMain.scala
@@ -7,27 +7,28 @@ import org.fs.rw.detection._
 import org.slf4s.Logging
 
 import com.typesafe.config.ConfigFactory
+import com.typesafe.config.Config
 
 object RouterWatcherMain extends App with Logging {
 
-  val config = ConfigFactory.parseFileAnySyntax(new File("application.conf"))
+  val config: Config = ConfigFactory.parseFileAnySyntax(new File("application.conf"))
   if (!config.hasPath("router")) {
     log.error("Config file not found or is invalid")
     scala.sys.exit(1)
   }
 
-  val detectors = Seq(
+  val detectors: Seq[Detector] = Seq(
     UpvelUR344AN4GPlus
   )
-  val routerDiscoverer = new RouterDiscoverer()
-  val executor = new DetectionExecutor(routerDiscoverer = routerDiscoverer, detectors = detectors)
-  val dao = new SlickDao(config = config)
+  val routerDiscoverer: RouterDiscoverer = new RouterDiscoverer()
+  val executor: org.fs.rw.DetectionExecutor = new DetectionExecutor(routerDiscoverer = routerDiscoverer, detectors = detectors)
+  val dao: org.fs.rw.database.SlickDao = new SlickDao(config = config)
   scala.sys.addShutdownHook {
     dao.tearDown()
     log.info("Shutdown complete")
   }
 
-  val iterator = new DetectionIterator(
+  val iterator: DetectionIterator = new DetectionIterator(
     routerDiscoverer = routerDiscoverer,
     executor = executor,
     dao = dao,
@@ -36,7 +37,7 @@ object RouterWatcherMain extends App with Logging {
 
   {
     import BuildInfo._
-    log.info(s"$name v$version started, awaiting 10 seconds")
+    log.info(s"$name v$version b${buildInfoBuildNumber} started, awaiting 10 seconds")
   }
   Thread.sleep(10 * 1000)
   iterator.start()
